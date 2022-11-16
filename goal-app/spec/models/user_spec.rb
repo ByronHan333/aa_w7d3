28d381d8b8af605f13da6962ca6f24a3d968f5c1
ByronHan333 <zhan12@dons.usfca.edu>
2022-11-16 13:35:32 -0800

    add

---
+++ goal-app/spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:valid_user) { create(:user) }
+
+  context 'validates' do
+    it 'presence of username' do
+      p valid_user
+      user = build(:user, username: '')
+      expect(user.save).to be(false)
+      expect(user.errors.full_messages).to eq(['Username can\'t be blank'])
+    end
+
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should have_many(:chirps) }
+  end
+end
